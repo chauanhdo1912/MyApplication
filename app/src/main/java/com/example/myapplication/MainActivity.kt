6ba7ee2776020a25e8e9e1cd339eade6ccd653e6
chauanhdo1912 <s_do23@stud.hwr-berlin.de>
2025-10-23 17:26:09 +0200

    1st draft

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        val helloButton = findViewById<Button>(R.id.Button)
+        helloButton.setText("Hello World")
+
+        helloButton.setOnClickListener {
+
+            onClick()
+
+
+        }
+    }
+
+    private fun onClick() {
+        val helloButton1 = findViewById<Button>(R.id.Button)
+        helloButton1.setText("Welt")
+
+
+
+    }
+
+
+}
+
+private fun MainActivity.setOnClickListener() {
+    TODO("Not yet implemented")
+}
+
+
+
+
+
+
+
